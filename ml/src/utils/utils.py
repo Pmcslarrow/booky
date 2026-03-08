a63888621d23b723d360edc43902844301e99020
Paul McSlarrow <pmcslarrow@icloud.com>
2026-03-08 17:36:26 -0500

    chore: 0001 refactoring folder structure for the feature branch

--- ml/src/utils/utils.py
+++ ml/src/utils/utils.py
@@ -1,5 +1,17 @@
 import torch
 
+def bucketize_age(age):
+    if age < 18:
+        return "<18"
+    elif age <= 25:
+        return "18-25"
+    elif age <= 35:
+        return "26-35"
+    elif age <= 50:
+        return "36-50"
+    else:
+        return"50+"
+
 def encode_user(user_id, age, encoders):
     user_id = encoders['User-ID'].get(str(user_id), 0)
     user_age = encoders['User-Age'].get(str(age), 0)
